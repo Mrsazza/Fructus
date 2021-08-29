809077b56f70fa2fcb66c6a6a447838c7c25b8df
Mrsazza <shahriarhossain147@gmail.com>
2021-08-29 16:37:06 +0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sazza on 29/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
